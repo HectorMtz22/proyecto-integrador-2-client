f45b752328fa6d6884b4fece1ac76344979c1428
Hector Mtz <hec.gre81@gmail.com>
2025-05-19 13:08:10 -0600

    Add fetching de datos

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,42 @@
+import { useState, useEffect } from 'react'
 import { Component } from '@/components/MyChart'
+import getData from './services/getData'
 import './App.css'
 
 function App() {
+  const [data, setData] = useState<any>(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    getData()
+      .then((data) => {
+        setData(data)
+        setLoading(false)
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error)
+        setError(error)
+        setLoading(false)
+      })
+  }, [])
+
   return (
-    <>
-      <div className='flex items-center flex-col w-full h-screen justify-center'>
+    <main className='flex flex-col w-full h-screen'>
+      {/* Header */}
+      <header className='flex items-center justify-between w-full h-16 px-10 text-white bg-green-600'>
         <h1>Dashboard</h1>
-        <div className='w-full max-w-2xl'>
-          <Component />
-        </div>
+      </header>
+      <div className='flex items-center flex-col w-full h-screen justify-center '>
+        {loading && <p className='text-2xl text-gray-500'>Cargando Dashboard...</p>}
+        {error && <p className='text-2xl text-red-500'>Error: {error}</p>}
+        {data && (
+          <div className='w-full max-w-2xl'>
+            <Component />
+          </div>
+        )}
       </div>
-    </>
+    </main>
   )
 }
 
---
+++ src/services/getData.ts
@@ -0,0 +1,15 @@
+const API_URL = 'http://localhost:5000'
+
+export default async function getData() {
+    const endpoint = `${API_URL}/data?`
+    const res = await fetch(endpoint + new URLSearchParams({
+        // Add any query parameters here
+        semestre: 'A2020'
+    }))
+    if (!res.ok) {
+        throw new Error('Failed to fetch data')
+    }
+    const data = await res.json()
+    console.log(data)
+    return data 
+}
